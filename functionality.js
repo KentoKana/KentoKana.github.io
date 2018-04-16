e1c2589ee3da8bc83bf6ae67feb239a241099c2e
Kento Kanazawa <33736354+KentoKana@users.noreply.github.com>
2018-04-15 17:41:04 -0700

    Added loading screen and fade-in effect on display

--- functionality.js
+++ functionality.js
@@ -1,5 +1,41 @@
+//Pre-Loading Screen====================================//
 
-//Clock display//
+var preload = document.getElementById('preloadTriforce');
+var bodyContent = document.getElementById('body');
+var navi = document.getElementById('navigation');
+var timeLoad = document.getElementById('time');
+var greetLoad = document.getElementById('greeting');
+var searchLoad = document.getElementById('searching');
+
+
+var loaded = false;
+
+function displayOff(){
+	if(loaded === false){
+		navi.style.display="none";
+		timeLoad.style.display="none";
+		greetLoad.style.display="none";
+		searchLoad.style.display="none";
+	} 
+}
+
+function pageLoaded(){
+	setInterval(function(){
+		loaded = true;
+		if(loaded === true && preload.className === "preloadImg"){
+			preload.className = "preloadOff";
+			document.body.style.display = ""
+			navi.style.display = "";
+			timeLoad.style.display="";
+			greetLoad.style.display="";
+			searchLoad.style.display="";
+			console.log('the page is loaded');
+		}
+	}, 4000)
+}
+
+
+//Clock display=======================================//
 function displayTime() {
 	var currentTime = new Date();
 	var hours = currentTime.getHours();
@@ -19,7 +55,7 @@ function displayTime() {
 
 setInterval(displayTime, 1000);
 
-//Greeting selector//
+//Greeting selector==================================//
 function selectGreetings() {
 
 	var currentTime = new Date();
@@ -41,11 +77,9 @@ function selectGreetings() {
 
 var greet = selectGreetings();
 
-console.log(greet)
-
 function displayGreeting() {
 	var name = 'Kento';
-	return document.getElementById('greeting').innerHTML = greet + name + '.';
+	document.getElementById('greeting').innerHTML = greet + name + '.';
 }
 
 var sayGreeting = displayGreeting()
@@ -53,16 +87,38 @@ var sayGreeting = displayGreeting()
 setInterval(displayGreeting, 1000);
 
 
-//Change background image based on greeting//
+//Change background image based on time===============//
 function changeBG() {
 	var currentTime = new Date();
 	var hours = currentTime.getHours();
 	if (hours >= 16 && hours <= 23 || hours >= 0 && hours <= 4) {
-		return  document.body.style.backgroundImage = "url('BoW-Night.jpg')";
+		document.body.style.backgroundImage = "url('BoW-Night.jpg')";
 	} else {
-		return document.body.style.backgroundImage = "url('BoW-Morning.jpg')";
+		document.body.style.backgroundImage = "url('BoW-Morning.jpg')";
 	}
 	
 }
 
 setInterval(changeBG, 1000);
+
+//Make Sheikah symbol glow when hovered over nav links//
+
+var animation = false;
+var background = document.getElementById("nav-backdrop");
+
+function glowOn() {
+	animation = true;
+	if (background.className === "sheikah") {
+		background.className += " active";
+	}
+}
+
+function glowOff() {
+	animation = false;
+	setTimeout(function() {
+		if (animation == false && background.className === "sheikah active") {
+			background.className = "sheikah";
+		}
+	}, 50)
+}
+
